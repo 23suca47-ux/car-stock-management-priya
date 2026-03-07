b826427af4f93b5406d92d09ca5eb0fb89537e4f
PRIYADARSHINI B <23suca47@tcarts.in>
2026-03-07 10:11:08 +0530

    Add files via upload

---
+++ CarstockManagingApplication.java
@@ -0,0 +1,12 @@
+package com.carstock.carstock_managing_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarstockManagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarstockManagingApplication.class, args);
+    }
+}
